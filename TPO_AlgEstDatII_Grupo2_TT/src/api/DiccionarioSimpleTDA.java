585460c11a504e3338c2c5995ff68d8bc8be02fd
augustoespinoz4 <129131293+augustoespinoz4@users.noreply.github.com>
2024-06-17 11:16:10 -0300

    Delete TPO_AlgEstDatII_Grupo2_TT directory

--- TPO_AlgEstDatII_Grupo2_TT/src/api/DiccionarioSimpleTDA.java
+++
@@ -1,9 +0,0 @@
-package api;
-
-public interface DiccionarioSimpleTDA<K, V> {
-    void InicializarDiccionario();
-    void Agregar(K clave, V valor);
-    void Eliminar(K clave);
-    V Recuperar(K clave);
-    ConjuntoTDA<K> Claves();
-}
--- TPO_AlgEstDatII_Grupo2_TT/src/api/PilaTDA.java
+++
@@ -1,9 +0,0 @@
-package api;
-
-public interface PilaTDA<T> {
-    void InicializarPila();
-    void Apilar(T x);
-    void Desapilar();
-    T Tope();
-    boolean PilaVacia();
-}
